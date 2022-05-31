ba3667c9acd569ce0a11a47a751d70d2519d631f
Gabriel Vazarini <camaravazarini@gmail.com>
2022-05-31 11:06:59 -0300

    Conversor

---
+++ ConversorDeMoeda/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConversorDeMoeda 
+{
+     class Conversor
+    {
+        public static double IOF = 0.06;
+        public  double DolarParaReal(double quantia, double valormoeda)
+        {
+            double valor = quantia * valormoeda;
+             return (valor * IOF) + valor;
+        }
+      
+     }
+}
---
+++ ConversorDeMoeda/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Globalization;
+using ConversorDeMoeda;
+namespace ProjConvert
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Conversor convert = new Conversor();
+            Console.WriteLine("Qual o valor da moeda desejada hoje? : ");
+            double valormoeda = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Qual a quantia que deseja comprar?: ");
+            double quantia = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            double result  = convert.DolarParaReal(valormoeda, quantia);
+            Console.WriteLine("O valor total a ser pago com IOF e de : " + result.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
